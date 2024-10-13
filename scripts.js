87563ffb8a7afe3763d9f24cd016935ab407694b
joel-uribe-fuentes <joel.uribe.fuentes@gmail.com>
2024-10-13 12:16:59 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,8 +8,3 @@ function showSlides() {
     slides[slideIndex].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
-
-function changeSlide(n) {
-    slideIndex += n - 1; // Adjust slideIndex
-    showSlides();
-}
